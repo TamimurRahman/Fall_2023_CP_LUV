9b9a40e6febb8b02e555e025a0b68ab537396f45
Tamimur <tamimr008@gmail.com>
2023-10-12 22:13:39 +0600

    Spoj online judge

---
+++ Test_life_The_universe_and_Everything.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num;
+    while(1){
+        cin>>num;
+        if(num==42){
+            break;
+        }
+        cout<<num<<endl;
+    }
+
+    return 0;
+}
