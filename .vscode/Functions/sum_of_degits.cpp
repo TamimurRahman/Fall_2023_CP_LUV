3bba1a27c8a85564ed786ffe7da0b34af421b68a
Tamimur <tamimr008@gmail.com>
2023-10-19 18:57:48 +0600

    Sum of digits using function two separate digits inputs and thear total sum

---
+++ .vscode/Functions/sum_of_degits.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum_digits(int);
+int main(){
+    int num1,num2;
+    cin>>num1>>num2;
+    int sum_digits_result = sum_digits(num1)+sum_digits(num2);
+    cout<<"Ans : "<<sum_digits_result<<endl;
+}
+int sum_digits(int num){
+    
+    int temp , sum = 0;
+    while(num!=0){
+        temp = num%10;
+        num = num/10;
+        sum = sum+temp;
+    }
+    return sum;
+}
